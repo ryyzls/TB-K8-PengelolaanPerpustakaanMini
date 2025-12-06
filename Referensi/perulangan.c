af2e9bd34f145f137644e30ef31a219dbd9f7787
Dwi2006 <dwichsnii@gmail.com>
2025-12-06 23:03:29 +0700

    Add looping program in perulangan.c

---
+++ Referensi/perulangan.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+// Kode warna
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
+// =======================
+// Dwi: PERULANGAN
+// =======================
+void loopProgram() {
+    int pilih;
+    do {
+        tampilMenu();           // Orang 1
+        scanf("%d", &pilih);
+        getchar();
+        prosesPilihan(pilih);   // Orang 3
+    } while(pilih != 0);
+}
+
