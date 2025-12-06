9ee0cbbcc1a961cbc926bedc6f279c88bf7b1f9f
rasydrtg <rasydrtg125@gmail.com>
2025-12-06 18:13:09 +0700

    Update fungsi.c

--- Referensi/fungsi.c
+++ Referensi/fungsi.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+// Kode warna
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
 void tambahbuku()
 {
   struct buku b;
@@ -35,4 +47,5 @@ printf("\n"BLUE"================================================================
     }
 printf(BLUE"======================================================================\n"RESET);
 }
+
 
