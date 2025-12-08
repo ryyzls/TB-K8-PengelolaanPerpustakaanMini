c08eb4c303d9a4c5a00cfd31799a3030bb2240ae
Rizal <fahriarizal@students.usu.ac.id>
2025-12-08 19:16:12 +0700

    Update K8_TB_PrakDaspro.c

--- K8_TB_PrakDaspro.c
+++ K8_TB_PrakDaspro.c
@@ -257,26 +257,7 @@ void simpanFile()
     printf(GREEN "Data berhasil disimpan ke file!\n" RESET);
 }
 
-void muatFile() {
-    FILE *file = fopen("buku.txt", "r");
-    if(file == NULL) {
-        printf(RED"File tidak ditemukan.\n"RED);
-        return;
-    }
 
-    jumlah = 0;
-    while(!feof(file)) {
-        struct Buku b;
-        if(fscanf(file, "%d|%49[^|]|%49[^|]|%d\n",
-            &b.id, b.judul, b.penulis, &b.tahun) == 4) {
-            daftar[jumlah] = b;
-            jumlah++;
-        }
-    }
-
-    fclose(file);
-    printf(GREEN"Data berhasil dimuat dari file!\n"RESET);
-}
 
 void prosesPilihan(int pilih) 
 {
