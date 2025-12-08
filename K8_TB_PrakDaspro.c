b2092cc11318ce75bdc6881cdc636c0c53ca414b
rasydrtg <rasydrtg125@gmail.com>
2025-12-08 09:31:50 +0700

    Update K8_TB_PrakDaspro.c
    Added functions to add and display books in the library system.

--- K8_TB_PrakDaspro.c
+++ K8_TB_PrakDaspro.c
@@ -54,3 +54,36 @@ void tampilMenu() {
     printf(MAGENTA"=======================================\n"RESET);
     printf(BLUE"Pilih Menu: "RESET);
 }
+void tambahBuku() {
+    struct Buku b;
+    b.id = jumlah + 1;
+
+    inputJudul(b.judul);
+    inputPenulis(b.penulis);
+    b.tahun = inputTahun();
+
+    daftar[jumlah] = b;
+    jumlah++;
+
+    printf(GREEN"Buku berhasil ditambahkan!\n"RESET);
+}
+
+void tampilkanBuku() {
+    if(jumlah == 0) {
+        printf(RED"Belum ada buku.\n"RESET);
+        return;
+    }
+
+    printf("\n"BLUE"======================================================================\n");
+    printf("| %-2s | %-30s | %-20s | %-4s |\n", "ID", "Judul Buku", "Penulis", "Tahun");
+    printf("======================================================================\n"RESET);
+
+    for(int i=0; i<jumlah; i++) {
+        printf(BLUE"|"RESET GREEN" %-2d "RESET BLUE"|"RESET GREEN" %-30s "RESET BLUE"|"RESET GREEN" %-20s "RESET BLUE"|"RESET GREEN" %-5d "RESET BLUE"|"RESET"\n",
+            daftar[i].id,
+            daftar[i].judul,
+            daftar[i].penulis,
+            daftar[i].tahun);
+    }
+    printf(BLUE"======================================================================\n"RESET);
+}
