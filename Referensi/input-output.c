cb6bed3e998bed963cb9e358c91dc6b677a7acb0
Dzakwan Rangkuti <zakwanrangkuti9@gmail.com>
2025-12-08 13:33:33 +0700

    Add input-output.c with color definitions

---
+++ Referensi/input-output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
